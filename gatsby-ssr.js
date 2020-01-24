e32276b52ee84d18d62345ecf708d7178d175219
Adao Lima <adaoolima@gmail.com>
2020-01-24 00:30:30 -0300

    Configuracoes de ambiente

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,7 @@
 /**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ * Implement Gatsby's SSR APIs in this file.
  *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ * See: https://www.gatsbyjs.org/docs/webpack-and-ssr/
  */
 
 // You can delete this file if you're not using it
