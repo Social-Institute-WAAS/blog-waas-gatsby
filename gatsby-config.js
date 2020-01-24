1db1957f80c2487dcc162918b9fa0c5f1682a890
Adao Lima <adaoolima@gmail.com>
2020-01-24 00:25:14 -0300

    Adicionadas configuracoes comentadas para depois verificar

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,8 +43,56 @@ module.exports = {
         // is it hosted on wordpress.com, or self-hosted?
         hostingWPCOM: false,
         // does your site use the Advanced Custom Fields Plugin?
-        useACF: false,
+        useACF: true,
+        auth: {
+          // If auth.user and auth.pass are filled, then the source plugin will be allowed
+          // to access endpoints that are protected with .htaccess.
+          // htaccess_user: 'your-htaccess-username',
+          // htaccess_pass: 'your-htaccess-password',
+          // htaccess_sendImmediately: false,
+          // If you use "JWT Authentication for WP REST API" (https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/)
+          // or (https://github.com/jonathan-dejong/simple-jwt-authentication) requires jwt_base_path, path can be found in WordPress wp-api.
+          // plugin, you can specify user and password to obtain access token and use authenticated requests against WordPress REST API.
+          // jwt_user: process.env.JWT_USER,
+          // jwt_pass: process.env.JWT_PASSWORD,
+          // jwt_base_path: '/jwt-auth/v1/token',
+          // Default - can skip if you are using https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/
+        },
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems.
         verboseOutput: false,
+        // Search and Replace Urls across WordPress content.
+        searchAndReplaceContentUrls: {
+          sourceUrl: 'http://localhost',
+          replacementUrl: 'https://127.0.0.1',
+        },
+        // Set how many simultaneous requests are sent at once.
+        concurrentRequests: 10,
+        // Set WP REST API routes whitelists
+        // and blacklists using glob patterns.
+        // Defaults to whitelist the routes shown
+        // in the example below.
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]`
+        // ` will either include or exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        // Whitelisted routes using glob patterns
+        // includedRoutes: [
+        //   '**/categories',
+        //   '**/posts',
+        //   '**/pages',
+        //   '**/media',
+        //   '**/tags',
+        //   '**/taxonomies',
+        //   '**/users',
+        // ],
+        // Blacklisted routes using glob patterns
+        // excludedRoutes: ['**/posts/1456'],
+        // Set this to keep media sizes.
+        // This option is particularly useful in case you need access to
+        // URLs for thumbnails, or any other media detail.
+        // Defaults to false
+        keepMediaSizes: false,
       },
     },
     {
